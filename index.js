f84c75d595850ef631f280fb5812f8fb32b451d9
RoyOkite <royokite@gmail.com>
2022-08-21 17:19:35 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// Code your solution here
+// Array filter method lab!
+
+function findMatching(drivers, nameFilter) {
+    return drivers.filter(driverName => driverName.toLowerCase()===nameFilter.toLowerCase());
+}
+
+function fuzzyMatch(drivers, stringSearch) {
+    return drivers.filter(driverName => driverName.slice(0, stringSearch.length).includes(stringSearch));
+}
+
+function matchName(drivers, driverName) {
+    return drivers.filter(driver => driver.name===driverName);
+}
